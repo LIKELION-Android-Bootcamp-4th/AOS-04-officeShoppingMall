55aa3193faad020e1b6e300530aac2cd06c73e76
DD <jah06114@naver.com>
2025-07-02 00:05:10 +0900

    refactor appBar

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -2,4 +2,7 @@ import 'package:flutter/animation.dart';
 
 class AppRoutes {
   static const String home = "/";
+  static const String login = " ";
+  static const String signup = " ";
+
 }
--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -6,22 +6,38 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final Widget? leading;
   final List<Widget> actions;
+  final bool centerTitle;
+  final TextStyle? titleTextStyle;
 
-  const CustomAppBar({super.key, this.title = AppConstants.appName, this.leading, this.actions = const []});
+  const CustomAppBar({
+    super.key,
+    this.title = AppConstants.appName,
+    this.leading,
+    this.actions = const [],
+    this.centerTitle = false,
+    this.titleTextStyle
+  });
 
   @override
   Widget build(BuildContext context) {
+
+    final TextStyle defaultTextStyle = GoogleFonts.roboto(
+      fontSize: title == AppConstants.appName ? 30 : 24,
+      fontWeight: FontWeight.w900,
+      fontStyle: FontStyle.italic,
+    );
+
     return AppBar(
       title: Text(
         title,
-        style: GoogleFonts.roboto(
-          fontSize: title == AppConstants.appName ? 30 : 24,
-          fontWeight: FontWeight.w900,
-          fontStyle: FontStyle.italic,
-        ),
+        style: titleTextStyle ?? defaultTextStyle
       ),
+      centerTitle: centerTitle,
       leading: leading,
       actions: actions,
+      elevation: 1,
+      shadowColor: Colors.black,
+      backgroundColor: Colors.white,
     );
   }
 
