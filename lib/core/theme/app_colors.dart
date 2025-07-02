2936cead933aaf84b5ca651aff057c21181cb88e
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-07-02 22:00:44 +0900

    feat: 앱 컬러 정의

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+class AppColors {
+  // 주요 브랜드 컬러
+  static const primaryColor = Color(0xFF2E77D6);
+  static const onPrimaryColor = Color(0xFFE5E5E5);
+  static const primaryContainerColor = Color(0xFF5595E8);
+
+  static const secondaryColor = Color(0xFF70AEFF);
+  static const onSecondaryColor = Color(0xFFFFFFFF);
+  static const secondaryContainerColor = Color(0xFF70AEFF);
+
+  static const tertiaryColor = Color(0x8270AEFF);
+  static const onTertiaryColor = Color(0xFFFFFFFF);
+
+  // 주요 배경 컬러
+  static const backgroundColor = Color(0xFFFCFDFF);
+  static const onBackgroundColor = Color(0xFF000000);
+
+  static const surfaceColor = Color(0xFFFCFDFF);
+  static const onSurfaceColor = Color(0xFF000000);
+  static const surfaceContainerColor = Color(0xFFF5F6FA);
+  static const onSurfaceContainerColor = Color(0xFF626262);
+
+  // 테두리, 디바이더
+  static const outlineColor = Color(0xFFDBDBDB);
+
+  // 그레이톤
+  static const gray900 = Color(0xFF1B1D1F);
+  static const gray600 = Color(0xFF454C53);
+  static const gray400 = Color(0xFF878EA1);
+  static const gray200 = Color(0xFFC9CDD2);
+  static const gray100 = Color(0xFFE8EBED);
+
+  // 에러
+  static const red900 = Color(0xFFFF402F);
+  static const red600 = Color(0xFFFF6450);
+}
