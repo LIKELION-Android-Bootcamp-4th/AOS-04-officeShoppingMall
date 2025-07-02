d233df13d9e58bb2b1882b86e2917d39dc2b7f3b
DD <jah06114@naver.com>
2025-07-02 09:12:09 +0900

    refactor routes 추가

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/animation.dart';
 
 class AppRoutes {
   static const String home = "/";
-  static const String login = " ";
-  static const String signup = " ";
+  static const String login = "/login";
+  static const String signup = "/signup";
 
 }
