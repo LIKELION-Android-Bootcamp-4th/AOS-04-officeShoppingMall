ee815ed4bbe4a35ba7ee2e8cc803efebf5b608aa
DD <jah06114@naver.com>
2025-07-02 22:36:31 +0900

    feat SignupScreen 추가

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/constants/app_constants.dart';
 import 'package:office_shopping_mall/core/constants/app_routes.dart';
-import 'package:office_shopping_mall/feature/home/ui/HomeScreen.dart';
+import 'package:office_shopping_mall/feature/auth/ui/signup_screen.dart';
 
 class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.home:
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return MaterialPageRoute(builder: (_) => SignupScreen());
 
       default:
         return MaterialPageRoute(
