1d518d959745ba42c3b3200f694b079cb802f7e6
groundinsider <groundinsider24@gmail.com>
2025-07-02 19:48:30 +0900

    상품 데이터 추가

--- lib/feature/product/bloc/product.dart
+++ lib/feature/product/bloc/product.dart
@@ -3,6 +3,7 @@ class Product{
   final String productName;
   final String price;
   final String imageUrl;
+  final int category;
 
-  Product({required this.id, required this.productName, required this.price, this.imageUrl = ''});
+  Product({required this.id, required this.productName, required this.price, this.imageUrl = '', required this.category});
 }
