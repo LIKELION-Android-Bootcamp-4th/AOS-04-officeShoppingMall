f3f74565374429ee17b5a529e23112034819e592
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-07-03 14:03:15 +0900

    feat: home 카테고리 버튼 구현

---
+++ lib/feature/home/ui/home_content_category.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeContentCategory extends StatelessWidget {
+  const HomeContentCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: List.generate(5, (index) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: () {}, icon: SvgPicture.asset('images/icon/ic_close.svg')),
+              Text('이름 ${index + 1}', style: Theme.of(context).textTheme.labelMedium),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
