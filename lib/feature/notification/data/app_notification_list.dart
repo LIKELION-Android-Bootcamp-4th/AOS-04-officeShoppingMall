be1fc68e74e4788e10949ec314302618b0aaa284
mninm <kyhee2001@naver.com>
2025-07-05 16:38:32 +0900

    알림 리스트 UI 생성

--- lib/feature/notification/data/app_notification_list.dart
+++ lib/feature/notification/data/app_notification_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/feature/notification/data/app_notification.dart';
 
 class AppNotificationList extends StatelessWidget {
-  List<AppNotification> notiList = [
+  final List<AppNotification> notiList = [
     AppNotification("공지 알림입니다", 1),
     AppNotification("배송 알림입니다", 2),
     AppNotification("광고 알림입니다", 3),
@@ -10,8 +10,21 @@ class AppNotificationList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-
+    return Container(
+      margin: EdgeInsets.all(16),
+      width: 360,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Color(0x4DD9D9D9),
+        borderRadius: BorderRadiusGeometry.circular(10),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text("테스트"),
+        )
+      )
     );
   }
 }
--- lib/feature/notification/ui/notification_screen.dart
+++ lib/feature/notification/ui/notification_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/widgets/app_bar_actions.dart';
 import 'package:office_shopping_mall/core/widgets/custom_app_bar.dart';
+import 'package:office_shopping_mall/feature/notification/data/app_notification_list.dart';
 import 'package:office_shopping_mall/feature/notification/ui/notification_navigation.dart';
 
 class NotificationScreen extends StatefulWidget {
@@ -37,7 +38,8 @@ class NotificationScreenState extends State<NotificationScreen> {
               children: [
                 Column(
                   children: [
-                    Text("전체 알림 탭 입니다")
+                    Text("전체 알림 탭 입니다"),
+                    AppNotificationList(),
                   ],
                 ),
                 Text("공지사항 알림 탭입니다"),
