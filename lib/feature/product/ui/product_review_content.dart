7cb66c94c1d8443440a817449fe2ffdaefbfb357
groundinsider <groundinsider24@gmail.com>
2025-07-06 13:25:10 +0900

    design: 비율 변경

--- lib/feature/product/ui/product_review_content.dart
+++ lib/feature/product/ui/product_review_content.dart
@@ -1,46 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/theme/app_colors.dart';
+import 'package:office_shopping_mall/feature/product/ui/product_content_container.dart';
 
 
 class ProductReviewContent extends StatelessWidget{
+  const ProductReviewContent({super.key});
 
-
+  @override
   Widget build(BuildContext context) {
     return Column(
-
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: [
-
-          GestureDetector(
-
-            onTap: () {
-
-            },
-
-            child: Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                height: 120,
-                decoration: BoxDecoration(
-                  color: AppColors.gray200,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-
-                child: Text(
-                  '상품 후기글 보러가기',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                )
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: ProductContentContainer(
+            width: double.infinity,
+            height: 120,
+            child: Center(
+              child: Text(
+                '상품 후기글 보러가기',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
             ),
           ),
-
-
-
-        ]
-
+        ),
+      ],
     );
-
   }
 
 }
