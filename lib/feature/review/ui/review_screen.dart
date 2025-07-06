c0503dbbe03eb52889a53b69e0615dd56871da22
mninm <kyhee2001@naver.com>
2025-07-06 21:57:30 +0900

    feat: 작성가능 탭 UI 생성 및 배송완료 상품 리스트 불러오기

---
+++ lib/feature/review/ui/review_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:office_shopping_mall/core/widgets/custom_tab_bar.dart';
+import 'package:office_shopping_mall/feature/review/data/review_list.dart';
+import 'package:office_shopping_mall/feature/setting/ui/noti_setting_tab.dart';
+import 'package:office_shopping_mall/feature/setting/ui/user_info_tab.dart';
+
+class ReviewScreen extends StatefulWidget {
+  const ReviewScreen({super.key});
+
+  @override
+  State<ReviewScreen> createState() => _ReviewScreenState();
+}
+
+class _ReviewScreenState extends State<ReviewScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "리뷰",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          customTabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(text: '작성 가능'),
+              Tab(text: '작성 완료'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                ReviewList(),
+                Text("이번"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
