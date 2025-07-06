3829f23a5cc36264c3bb6df3959b3d4d756cc5e0
groundinsider <groundinsider24@gmail.com>
2025-07-06 13:22:51 +0900

    feat: 데이터 전달 방식 변경

--- lib/feature/product/data/product.dart
+++ lib/feature/product/data/product.dart
@@ -19,12 +19,25 @@ void setSelectProductId(int? productId) {
 
 int? getSelectProductId() => _selectProductId;
 
+Product? getSelectedProductData() {
+  final id = getSelectProductId();
+  if (id == null) return null;
+
+  try {
+    return products.firstWhere((element) => element.id == id);
+  } catch (e) {
+    return null;
+  }
+}
+
 var products = List<Product>.generate(60, (index) => Product(
   id: index,
   productName: "상품명",
   price: 10000 + index,
   category: 0,
+  categoryInfo: {"td": "asd"}
 ),
 );
+
 
 
