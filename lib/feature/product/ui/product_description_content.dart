c98153451d8d623c30698247304cf49d384c3d2d
groundinsider <groundinsider24@gmail.com>
2025-07-06 13:23:18 +0900

    feat: 상품 상세 설명 추가

--- lib/feature/product/ui/product_description_content.dart
+++ lib/feature/product/ui/product_description_content.dart
@@ -1,44 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/theme/app_colors.dart';
+import 'package:office_shopping_mall/feature/product/ui/product_content_container.dart';
 
 import '../data/product.dart';
 
-Product productData = products.firstWhere((element) => element.id == getSelectProductId());
-
 class ProductDescriptionContent extends StatelessWidget {
   const ProductDescriptionContent({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-
       children: [
-        Container(
+        ProductContentContainer(
           width: double.infinity,
-
           constraints: BoxConstraints(
             minHeight: 200,
           ),
-
-          padding: EdgeInsets.all(16),
-          alignment: Alignment.topLeft,
-
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: AppColors.gray200,
-            boxShadow: [
-              BoxShadow(
-                color: AppColors.gray200,
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: Offset(0, 2)
-              )
-            ]
-          ),
-
           child: Text(
             '상품 정보가 없습니다',
             style: Theme.of(context).textTheme.bodyLarge,
