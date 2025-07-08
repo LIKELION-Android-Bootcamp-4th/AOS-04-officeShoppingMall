f55f44feac8dfd5f5c89302ff6c2798d5fcead53
DD <jah06114@naver.com>
2025-07-08 09:09:01 +0900

    chore: 앱 시작 지점 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/constants/app_constants.dart';
-import 'package:office_shopping_mall/feature/auth/data/auth_service.dart';
-import 'package:office_shopping_mall/feature/auth/presentation/viewmodel/auth_view_model.dart';
 import 'package:office_shopping_mall/feature/cart/data/product_provider.dart';
 import 'package:office_shopping_mall/core/providers/bottom_nav_provider.dart';
 import 'package:provider/provider.dart';
 import 'app_router.dart';
 import 'core/constants/app_routes.dart';
+import 'core/data/network/api_client.dart';
 import 'core/theme/theme.dart';
+import 'feature/auth/auth_module.dart';
+import 'feature/mypage/mypage_module.dart';
 
 void main() {
   runApp(const WalkinApp());
@@ -24,15 +25,14 @@ class WalkinApp extends StatelessWidget {
         Provider<Dio>(create: (_) => ApiClient.dio),
         ChangeNotifierProvider(create: (_) => BottomNavProvider()),
         ChangeNotifierProvider(create: (_) => ProductProvider()),
-        ...mypageModule
+        ...mypageModule,
         ...authModule,
       ],
       child: MaterialApp(
         title: AppConstants.appName,
         theme: appThemeData(),
         debugShowCheckedModeBanner: false,
-        //TODO: login으로 바꾸기
-        initialRoute: AppRoutes.home,
+        initialRoute: AppRoutes.login,
         onGenerateRoute: AppRouter.onGenerateRoute,
       ),
     );
