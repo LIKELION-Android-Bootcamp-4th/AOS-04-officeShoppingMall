c6d97ac677e727912072669974d4227c2b7b20e7
DD <jah06114@naver.com>
2025-07-08 11:54:26 +0900

    feat: authRepository에 login추가

--- lib/feature/auth/domain/auth_repository.dart
+++ lib/feature/auth/domain/auth_repository.dart
@@ -1,3 +1,5 @@
+import 'package:office_shopping_mall/core/data/models/auth/login_request.dart';
+import 'package:office_shopping_mall/core/data/models/auth/login_response.dart';
 import '../../../core/data/models/auth/signup_request.dart';
 import '../../../core/data/models/auth/signup_response.dart';
 import '../data/auth_service.dart';
@@ -16,4 +18,14 @@ class AuthRepository{
       rethrow;
     }
   }
+
+  Future<LoginResponse> logIn(LoginRequest request) async{
+    try{
+      final response = await _service.loginAction(requestData: request);
+      return response;
+    } catch (e){
+      rethrow;
+    }
+  }
+
 }
