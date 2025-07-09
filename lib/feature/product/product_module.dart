c28c9b3aba7a298ae42b29e3b125d1f6df01602d
groundinsider <groundinsider24@gmail.com>
2025-07-09 16:37:34 +0900

    feat: 상태 관리 적용

---
+++ lib/feature/product/product_module.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:office_shopping_mall/feature/product/data/product_service.dart';
+import 'package:office_shopping_mall/feature/product/domain/repository/product_repository.dart';
+import 'package:office_shopping_mall/feature/product/presentation/viewmodel/product_list_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import 'data/product_viewmodel.dart';
+import 'domain/product_repository_impl.dart';
+
+List<SingleChildWidget> productModule = [
+  Provider<ProductService>(create: (context) => ProductService(context.read<Dio>())),
+
+  Provider<ProductRepository>(create: (context) => ProductRepositoryImpl(context.read<ProductService>())),
+
+  ChangeNotifierProvider<ProductListViewModel>(
+    create: (context) => ProductListViewModel(context.read<ProductRepository>())..loadProducts(),
+  ),
+  ChangeNotifierProvider<ProductDataViewModel>(
+    create: (context) => ProductDataViewModel(context.read<ProductRepository>()),
+  ),
+];
