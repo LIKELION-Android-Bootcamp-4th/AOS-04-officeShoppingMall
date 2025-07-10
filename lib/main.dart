485482d6b339abeb25c732c78bc21fb7342845b0
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-07-10 12:00:16 +0900

    chore: 기타 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/constants/app_constants.dart';
+import 'package:office_shopping_mall/core/utils/bottom_nav_observer.dart';
+import 'package:office_shopping_mall/feature/cart/data/cart_provider.dart';
+import 'package:office_shopping_mall/feature/cart/data/order_provider.dart';
 import 'package:office_shopping_mall/feature/cart/data/product_provider.dart';
 import 'package:office_shopping_mall/core/providers/bottom_nav_provider.dart';
+import 'package:office_shopping_mall/feature/product/product_module.dart';
+import 'package:office_shopping_mall/feature/search/search_module.dart';
 import 'package:office_shopping_mall/feature/setting/setting_module.dart';
 import 'package:provider/provider.dart';
 import 'app_router.dart';
