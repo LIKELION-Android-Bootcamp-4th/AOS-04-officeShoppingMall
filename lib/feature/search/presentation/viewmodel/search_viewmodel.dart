26ae318d42db8f88077c6799cc8f43f9f76a5b34
DD <jah06114@naver.com>
2025-07-10 14:45:50 +0900

    feat: 검색과 관련해 DTO 수정 및  service, repository, viewModel 구현
    # Conflicts:
    #	lib/core/constants/api_endpoints.dart
    #	lib/core/data/models/dto/product.dart

--- lib/feature/search/presentation/viewmodel/search_viewmodel.dart
+++ lib/feature/search/presentation/viewmodel/search_viewmodel.dart
@@ -1,29 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:office_shopping_mall/core/data/models/dto/product.dart';
 import 'package:office_shopping_mall/feature/search/domain/search_repository.dart';
 
 import '../../data/popular/popular_data_dto.dart';
 
-class SearchViewModel extends ChangeNotifier{
+class SearchViewModel extends ChangeNotifier {
   final SearchRepository _repository;
 
   PopularDataDto? popularDataDto;
   bool isLoading = false;
   String? error;
+  List<Product> searchResult = [];
 
   SearchViewModel(this._repository);
 
-  Future<void> getPopularKeywords() async{
+  Future<void> searchProducts(String query) async {
+    isLoading = true;
+    error = null;
+    searchResult = [];
+    notifyListeners();
+
+    try {
+      searchResult = await _repository.searchProducts(query);
+    } catch (e) {
+      error = "상품 검색 실패 $e";
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> getPopularKeywords() async {
     isLoading = true;
     error = null;
     notifyListeners();
 
-    try{
+    try {
       popularDataDto = await _repository.getPopularKeyword();
     } catch (e) {
       error = "인기 검색어 로드 실패 $e";
-    }finally{
+    } finally {
       isLoading = false;
-      notifyListeners();;
+      notifyListeners();
+      ;
     }
   }
 }
