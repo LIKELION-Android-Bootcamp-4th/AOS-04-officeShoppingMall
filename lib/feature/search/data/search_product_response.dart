30b388a9a9acbdbaf84286c1d4d9711da4124caa
DD <jah06114@naver.com>
2025-07-10 13:52:44 +0900

    refactor: 괄호 빠진거 추가..

--- lib/feature/search/data/search_product_response.dart
+++ lib/feature/search/data/search_product_response.dart
@@ -11,13 +11,13 @@ class SearchProductResponse {
     required this.items,
   });
 
-  factory SearchProductResponse.fromJson(Map<String, dynamic> json){
+  factory SearchProductResponse.fromJson(Map<String, dynamic> json) {
     return SearchProductResponse(
-        success: json['success'] as bool? ?? false,
-        messgae: json['message'] as String? ?? '',
-        items: (json['data']?['items'] as List<dynamic> ?? []).map((e) =>
-            Product.fromJson(e)).toList();
+      success: json['success'] as bool? ?? false,
+      messgae: json['message'] as String? ?? '',
+      items: (json['data']?['items'] as List<dynamic>? ?? [])
+          .map((e) => Product.fromJson(e))
+          .toList(),
     );
   }
-
 }
