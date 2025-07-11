4a1d8be1b85b1cf85ab08f513d67d4b997d84a03
DD <jah06114@naver.com>
2025-07-11 21:28:56 +0900

    feat: 검색 api 추가 (테스트완료)

--- lib/feature/search/presentation/search_screen.dart
+++ lib/feature/search/presentation/search_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:office_shopping_mall/feature/search/presentation/viewmodel/search_viewmodel.dart';
 import 'package:office_shopping_mall/feature/search/presentation/widgets/search_content_searchbar.dart';
 import 'package:office_shopping_mall/feature/search/presentation/widgets/search_popular.dart';
 import 'package:office_shopping_mall/feature/search/presentation/widgets/search_recent_keyword.dart';
+
+import '../../product/presentation/product_list_screen.dart';
+import '../data/search_service.dart';
+import '../domain/search_repository.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -13,8 +18,30 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final GlobalKey<SearchRecentKeywordState> _recentKeywordKey = GlobalKey();
 
-  void _onSearchSubmitted(String keyword) {
+  void _onSearchSubmitted(String keyword) async {
     _recentKeywordKey.currentState?.addKeyword(keyword);
+
+    final viewModel = SearchViewModel(SearchRepository(SearchService()));
+
+    try {
+      await viewModel.searchProducts(keyword);
+      final results = viewModel.searchResult;
+
+      if (!context.mounted) return;
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ProductListScreen(searchResults: results),
+        ),
+      );
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("검색 실패: $e")),
+        );
+      }
+    }
   }
 
   void _onKeywordSelected(String keyword) {
