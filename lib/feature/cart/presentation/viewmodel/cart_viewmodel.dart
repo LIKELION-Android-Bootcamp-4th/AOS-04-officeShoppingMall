a8269ee2242d39bb228ae437063b6d3497d1ee95
mninm <kyhee2001@naver.com>
2025-07-11 14:52:10 +0900

    feat: 뷰모델 추가

---
+++ lib/feature/cart/presentation/viewmodel/cart_viewmodel.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:office_shopping_mall/core/data/models/dto/cart_dto.dart';
+import 'package:office_shopping_mall/feature/cart/data/cart_service.dart';
+import 'package:office_shopping_mall/feature/cart/domian/repository/cart_repository.dart';
+
+class CartViewModel extends ChangeNotifier {
+  final CartRepository _repository;
+  final CartService _service;
+
+  List<CartDTO> carts = [];
+  bool _isLoading = false;
+  final Set<String> _selectedCartIds = {};
+
+  CartViewModel(this._repository, this._service);
+
+  List<CartDTO> get selectedCarts => carts.where((carts) => _selectedCartIds.contains(carts.cartId)).toList();
+  bool isSelected(String cartId) => _selectedCartIds.contains(cartId);
+
+  Future<void> loadCarts() async {
+    _isLoading = true;
+    carts.clear();
+    notifyListeners();
+
+    try {
+      final cartList = await _repository.getCart();
+      carts.addAll(cartList);
+    } catch (e) {
+      print('불러오기 실패: $e');
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteCart(String cartId) async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      await _service.deleteCart(cartId);
+      carts.removeWhere((cart) => cart.cartId == cartId);
+    } catch (e) {
+      print('삭제 실패: $e');
+    }
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  void toggleCarts (String cartId) {
+    if (_selectedCartIds.contains(cartId)) {
+      _selectedCartIds.remove(cartId);
+    } else {
+      _selectedCartIds.add(cartId);
+    }
+    notifyListeners();
+  }
+
+  void clearCarts() {
+    _selectedCartIds.clear();
+    notifyListeners();
+  }
+
+  int get selectedTotalPrice {
+    return selectedCarts.fold(0, (sum, carts) => sum + carts.product.price);
+  }
+}
