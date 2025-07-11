a31e217457f9c3e2d9994e60781c3f8ea9174c79
mninm <kyhee2001@naver.com>
2025-07-11 22:16:48 +0900

    feat: 뷰모델 추가
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:office_shopping_mall/feature/cart/presentation/cart_module.dart'
 import 'package:office_shopping_mall/feature/home/home_module.dart';
 import 'package:office_shopping_mall/feature/order/order_module.dart';
 import 'package:office_shopping_mall/feature/preference/preference_module.dart';
+import 'package:office_shopping_mall/feature/order/order_module.dart';
 import 'package:office_shopping_mall/feature/product/product_module.dart';
 import 'package:office_shopping_mall/feature/search/search_module.dart';
 import 'package:office_shopping_mall/feature/setting/setting_module.dart';
