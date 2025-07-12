20b5fe3be08aadf2683eebfae8a2a29efac9c335
groundinsider <groundinsider24@gmail.com>
2025-07-12 12:30:29 +0900

    feat: 상태 관리 적용

---
+++ lib/feature/review/presentation/viewmodel/review_model.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:office_shopping_mall/core/data/models/dto/review_dto.dart';
+import '../../../../core/data/models/dto/product_dto.dart';
+import '../../../../core/data/models/entity/user.dart';
+import '../../../product/presentation/viewmodel/product_viewmodel.dart';
+import '../../data/review_repository.dart';
+
+class ReviewModel extends ChangeNotifier{
+  final TextEditingController reviewController = TextEditingController();
+  final List<SvgPicture?> score = List.generate(5, (index) => SvgPicture.asset(""));
+  int scoreValue = 0;
+  final List<XFile?> images = [];
+  final ImagePicker _picker = ImagePicker();
+  final int visibleCount = 3;
+
+  final ReviewRepository _reviewRepository;
+
+  ReviewModel(this._reviewRepository);
+
+  void onScoreSelected(int index) {
+    for(int i = 0; i < score.length; i++) {
+      if (i <= index) {
+        score[index] = SvgPicture.asset("");
+      }
+      else {
+        score[index] = SvgPicture.asset("");
+      }
+    }
+    scoreValue = index + 1;
+    notifyListeners();
+  }
+
+  Future<void> pickImage(int index) async {
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      if (index < images.length) {
+        images[index] = pickedFile;
+      } else {
+        images.add(pickedFile);
+      }
+
+    }
+    notifyListeners();
+  }
+
+  ReviewDTO createReview({required ProductDTO product, required User user}) {
+    final reviewDTO = ReviewDTO(
+      id: "",
+      productId: product.id,
+      userId: user.id,
+      rating: scoreValue,
+      comment: reviewController.text,
+      createdAt: DateTime.now(),
+    );
+
+    return reviewDTO;
+  }
+
+  Future<void> addReview(ReviewDTO reviewDTO) async {
+    await _reviewRepository.addReview(reviewDTO, reviewDTO.productId);
+  }
+
+  Future<ReviewDTO> getReviews(String productId) async {
+    return await _reviewRepository.getReviews(productId);
+  }
+}
