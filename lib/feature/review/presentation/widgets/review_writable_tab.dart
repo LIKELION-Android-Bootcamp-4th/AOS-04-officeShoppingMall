ad023849d47859c091e93cf06a517754d97cc487
mninm <kyhee2001@naver.com>
2025-07-14 01:08:49 +0900

    refactor: 작성가능 탭 재수정

--- lib/feature/review/presentation/widgets/review_writable_tab.dart
+++ lib/feature/review/presentation/widgets/review_writable_tab.dart
@@ -8,36 +8,49 @@ import 'package:office_shopping_mall/feature/review/presentation/add_review_scre
 class ReviewWritableTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final orderIndex3 = context.watch<OrderViewModel>().orders.where((a) => a.orderIndex == 3).toList();
+    final orderIndex3 = context
+        .watch<OrderViewModel>()
+        .orders
+        .where((a) => a.orderIndex == 3)
+        .toList();
+
     return Align(
       alignment: Alignment.topCenter,
       child: Column(
         children: [
-          OrderList(
-            orders: orderIndex3
-          ),
-          orderIndex3.isNotEmpty ?
-          Container(
-            width: MediaQuery.of(context).size.width * 0.92,
-            height: MediaQuery.of(context).size.height * 0.06,
-            decoration: BoxDecoration(
-              color: AppColors.surfaceContainerColor,
-              borderRadius: BorderRadiusGeometry.circular(10),
-            ),
-            child: InkWell(
+          if (orderIndex3.isNotEmpty) ...[
+            OrderList(orders: orderIndex3),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.92,
+              height: MediaQuery.of(context).size.height * 0.06,
+              decoration: BoxDecoration(
+                color: AppColors.surfaceContainerColor,
+                borderRadius: BorderRadiusGeometry.circular(10),
+              ),
+              child: InkWell(
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (_) => AddReviewScreen()), //리뷰 작성 페이지로 이동
+                    MaterialPageRoute(builder: (_) => AddReviewScreen()),
                   );
                 },
                 child: Align(
                   alignment: Alignment.center,
-                  child: Text('리뷰 작성', style: Theme.of(context).textTheme.bodyLarge),
-                )
+                  child: Text(
+                    '리뷰 작성',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+              ),
             ),
-          ):
-          Text('작성 가능한 리뷰가 없습니다.', style: Theme.of(context).textTheme.bodyLarge,)
+            SizedBox(height: 20)
+          ] else
+            Center(
+              child: Text(
+                '작성 가능한 리뷰가 없습니다.',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
         ],
       ),
     );
