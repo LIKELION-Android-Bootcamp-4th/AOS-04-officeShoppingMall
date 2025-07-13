11ded0d6f171f1ccb5cfd1935e2c36ee07adbdab
mninm <kyhee2001@naver.com>
2025-07-13 23:38:17 +0900

    refactor: 작성가능 리뷰 페이지 수정

--- lib/feature/review/presentation/widgets/review_writable_tab.dart
+++ lib/feature/review/presentation/widgets/review_writable_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:office_shopping_mall/core/theme/app_colors.dart';
 import 'package:office_shopping_mall/feature/order/data/order_list.dart';
 import 'package:office_shopping_mall/feature/order/presentation/viewmodel/order_viewmodel.dart';
 import 'package:office_shopping_mall/feature/review/presentation/add_review_screen.dart';
@@ -7,22 +8,20 @@ import 'package:office_shopping_mall/feature/review/presentation/add_review_scre
 class ReviewWritableTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final orderIndex3 = context.watch<OrderViewModel>().orders.where((a) => a.orderIndex == 3).toList();
     return Align(
       alignment: Alignment.topCenter,
       child: Column(
         children: [
           OrderList(
-            orders: context
-                .watch<OrderViewModel>()
-                .orders
-                .where((a) => a.orderIndex == 3)
-                .toList(),
+            orders: orderIndex3
           ),
+          orderIndex3.isNotEmpty ?
           Container(
             width: MediaQuery.of(context).size.width * 0.92,
             height: MediaQuery.of(context).size.height * 0.06,
             decoration: BoxDecoration(
-              color: Color(0x4DD9D9D9),
+              color: AppColors.surfaceContainerColor,
               borderRadius: BorderRadiusGeometry.circular(10),
             ),
             child: InkWell(
@@ -34,10 +33,11 @@ class ReviewWritableTab extends StatelessWidget {
                 },
                 child: Align(
                   alignment: Alignment.center,
-                  child: Text("리뷰 작성", style: TextStyle(fontSize: 20),),
+                  child: Text('리뷰 작성', style: Theme.of(context).textTheme.bodyLarge),
                 )
             ),
-          ),
+          ):
+          Text('작성 가능한 리뷰가 없습니다.', style: Theme.of(context).textTheme.bodyLarge,)
         ],
       ),
     );
