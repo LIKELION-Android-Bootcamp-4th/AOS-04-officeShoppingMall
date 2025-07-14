c2bec1d293491b01b4be676a55826a9b3b9c352c
mninm <kyhee2001@naver.com>
2025-07-14 11:28:38 +0900

    refactor: 미사용 import 제거

--- lib/feature/notification/presentation/viewmodel/notification_viewmodel.dart
+++ lib/feature/notification/presentation/viewmodel/notification_viewmodel.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:office_shopping_mall/core/data/models/dto/notification_dto.dart';
 import 'package:office_shopping_mall/feature/notification/data/notification_repository.dart';
-import 'package:office_shopping_mall/feature/notification/data/notification_service.dart';
 
 class NotificationViewModel extends ChangeNotifier {
   final NotificationRepository _repository;
