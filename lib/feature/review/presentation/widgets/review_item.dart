095ab1783a4a7e747ca2fad2ea88d5b38f435a09
groundinsider <groundinsider24@gmail.com>
2025-07-15 13:34:19 +0900

    feat: 상테 관리 적용

--- lib/feature/review/presentation/widgets/review_item.dart
+++ lib/feature/review/presentation/widgets/review_item.dart
@@ -13,48 +13,58 @@ class ReviewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProductContentContainer(
-      width: double.infinity,
+    return Padding(
 
-      color: AppColors.gray200,
-      child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        // 별점
-        SvgPicture.asset(""),
-        Text(
-          "${review.rating}",
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-        const SizedBox(height: 4),
-        // 후기 내용
-        Text(
-          review.comment.isEmpty ? "내용이 없습니다." : review.comment,
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
-        const SizedBox(height: 8),
-        // 이미지
-        if (review.images != null && review.images!.isNotEmpty)
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                for (var image in review.images!) ...[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      image.url,
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(width: 2),
-                ],
-              ],
+      padding: const EdgeInsets.symmetric(vertical: 8),
+
+      child: ProductContentContainer(
+        width: double.infinity,
+
+        color: AppColors.gray200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 별점
+            Text(
+              "${review.rating}",
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .titleLarge,
             ),
-          )
-      ],
+            const SizedBox(height: 4),
+            // 후기 내용
+            Text(
+              review.comment.isEmpty ? "내용이 없습니다." : review.comment,
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .bodyMedium,
+            ),
+            const SizedBox(height: 8),
+            // 이미지
+            if (review.images != null && review.images!.isNotEmpty)
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    for (var image in review.images!) ...[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          image.url,
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(width: 2),
+                    ],
+                  ],
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
