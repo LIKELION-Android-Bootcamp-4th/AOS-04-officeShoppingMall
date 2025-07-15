415f61a20cf391c4418a6e45f03709ee92538f56
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-07-15 21:36:43 +0900

    fix: 로그인 화면에서 뒤로갈 시 앱이 종료되도록 루트 화면 변경

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,8 +1,8 @@
 class AppRoutes {
-  static const String home = '/';
+  static const String splash = '/';
+  static const String home = '/home';
   static const String login = '/login';
   static const String signup = '/signup';
-  static const String splash = '/splash';
   static const String category = '/category';
   static const String preference = '/preference';
   static const String productList = '/productList';
