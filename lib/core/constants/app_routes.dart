8424358bf6a571471f0f2cdb8dc0844bc8f06983
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-07-15 21:17:46 +0900

    feat: 스플래시 루트 지정

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -2,6 +2,7 @@ class AppRoutes {
   static const String home = '/';
   static const String login = '/login';
   static const String signup = '/signup';
+  static const String splash = '/splash';
   static const String category = '/category';
   static const String preference = '/preference';
   static const String productList = '/productList';
