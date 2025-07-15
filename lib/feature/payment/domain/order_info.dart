ba3133b94d767d94dddce785b8ac21a9c0f4d3f4
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-07-15 18:06:56 +0900

    feat: 주문 필요 정보에 따라 추가 정보 구현

--- lib/feature/payment/domain/order_info.dart
+++ lib/feature/payment/domain/order_info.dart
@@ -1,12 +1,16 @@
 class OrderInfo {
-  final String paymentMethod;
+  final String recipient;
   final String address;
+  final String phone;
+  final String paymentMethod;
   final int quantity;
   final int unitPrice;
 
   OrderInfo({
-    required this.paymentMethod,
+    required this.recipient,
     required this.address,
+    required this.phone,
+    required this.paymentMethod,
     required this.quantity,
     required this.unitPrice,
   });
